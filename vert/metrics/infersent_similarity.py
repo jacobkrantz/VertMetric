c5f711e279cf850e6991ced06a106fe213643413
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2018-07-30 19:00:04 -0600

    Begin to implement Infersent Similarity

--- vert/metrics/infersent_similarity.py
+++ vert/metrics/infersent_similarity.py
@@ -1,8 +1,10 @@
 
 import logging
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
 
 from vert.metrics import metric
-from vert.utils import general
+from vert.utils import general as gen
 
 
 class InfersentSimilarity(metric.Metric):
@@ -13,13 +15,29 @@ class InfersentSimilarity(metric.Metric):
     def score(self, make_report=True):
         self.logger.debug("Calculating Infersent Similarity scores.")
         self.logger.warn("Not implemened yet.")
-        general.check_data_loaded(self.generated, self.targets)
+        gen.check_data_loaded(self.generated, self.targets)
 
-        sim = 0.0
+        infersent = torch.load(
+            "./data/infersent.allnli.pickle",
+            map_location=lambda storage,
+            loc: storage
+        )
+        infersent.set_glove_path("./data/glove.840B.300d.txt")
+        infersent.build_vocab_k_words(1000000)
+
+        embeddings_1 = infersent.encode(self.generated, tokenize=True)
+        self.logger.debug('InferSent: Embeddings generated for group 1.')
+        embeddings_2 = infersent.encode(self.targets, tokenize=True)
+        self.logger.debug('InferSent: Embeddings generated for group 2.')
+
+        sim = np.mean(list(starmap(
+            lambda e1,e2: self._cos_sim(e1,e2),
+            zip(embeddings_1, embeddings_2)
+        )))
 
         self.logger.debug("Done: calculating Infersent Similarity scores.")
         if make_report:
-            return self.generate_report(sim=sim)
+            return self.generate_report(sim=gen.fmt_rpt_line(sim))
         return sim
 
     @classmethod
@@ -32,5 +50,14 @@ class InfersentSimilarity(metric.Metric):
             None
         """
         if filename == '':
-            filename = general.generate_filename('sim')
+            filename = gen.generate_filename('sim')
         super(InfersentSimilarity, cls).save_report_to_file(report, out_dir, filename)
+
+    def _cos_sim(self, sentence_1, sentence_2):
+        """
+        Only call with one sentence at a time.
+        """
+        return cosine_similarity(
+            sentence_1.reshape(1,-1),
+            sentence_2.reshape(1,-1)
+        )[0][0]
